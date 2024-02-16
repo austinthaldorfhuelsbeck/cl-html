0671000f941482c2696628f3fd5066f1a0ee619f
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-02-15 18:17:32 -0800

    the api is live on vercel

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,5 @@
 async function fetchData(apiPath) {
-	const response = await fetch(`http://localhost:7070/${apiPath}`);
+	const response = await fetch(`https://website-builder-server-lac.vercel.app/${apiPath}`);
 	if (!response.ok) throw new Error("Network response was not ok");
 	return response.json();
 }
