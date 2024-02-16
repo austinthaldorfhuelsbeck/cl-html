1e6c82c98e6d98cde83e08b813da00915067c99f
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-02-15 19:00:16 -0800

    the api is live on heroku

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,5 @@
 async function fetchData(apiPath) {
-	const response = await fetch(`https://website-builder-server-lac.vercel.app/${apiPath}`);
+	const response = await fetch(`https://website-builder-server-6b518a81afd2.herokuapp.com/${apiPath}`);
 	if (!response.ok) throw new Error("Network response was not ok");
 	return response.json();
 }
