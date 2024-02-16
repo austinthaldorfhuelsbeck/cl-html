8bd5e99f9dff0dfb413dbad1e9d22f1216564ae8
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-02-15 19:31:52 -0800

    proper api route

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,5 @@
 async function fetchData(apiPath) {
-	const response = await fetch(`https://website-builder-server-6b518a81afd2.herokuapp.com/${apiPath}`);
+	const response = await fetch(`https://website-builder-server-6b518a81afd2.herokuapp.com/api/v1/${apiPath}`);
 	if (!response.ok) throw new Error("Network response was not ok");
 	return response.json();
 }
